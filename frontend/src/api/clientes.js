80273a254a6cbeefbf5bc44b79d430b6edf857d8
LeninCampos <2blenincampos@gmail.com>
2025-11-26 12:40:47 -0600

    reparacion de clientes

--- frontend/src/api/clientes.js
+++ frontend/src/api/clientes.js
@@ -3,13 +3,18 @@ import axios from "axios";
 
 const API_URL = "http://localhost:4000/api/clientes";
 
-// Si luego usas login/JWT puedes adaptar esto igual que en productos
+function getAuthHeaders() {
+  const token = localStorage.getItem("token");
+  return token ? { Authorization: `Bearer ${token}` } : {};
+}
+
 export const getClientes = () => axios.get(API_URL);
 
-export const createCliente = (data) => axios.post(API_URL, data);
+export const createCliente = (data) => 
+  axios.post(API_URL, data, { headers: getAuthHeaders() });
 
 export const updateCliente = (id, data) =>
-  axios.put(`${API_URL}/${id}`, data);
+  axios.put(`${API_URL}/${id}`, data, { headers: getAuthHeaders() });
 
 export const deleteCliente = (id) =>
-  axios.delete(`${API_URL}/${id}`);
+  axios.delete(`${API_URL}/${id}`, { headers: getAuthHeaders() });
