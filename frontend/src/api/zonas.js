439275cce59c5b63a5847a94128c57f11ecc7c67
LeninCampos <2blenincampos@gmail.com>
2025-11-26 17:49:02 -0600

    tokens en zonas

--- frontend/src/api/zonas.js
+++ frontend/src/api/zonas.js
@@ -3,7 +3,14 @@ import axios from "axios";
 
 const API_URL = "http://localhost:4000/api/zonas";
 
+function getAuthHeaders() {
+  const token = localStorage.getItem("token");
+  return token ? { Authorization: `Bearer ${token}` } : {};
+}
+
 export const getZonas = () => axios.get(API_URL);
-export const createZona = (data) => axios.post(API_URL, data);
-export const updateZona = (id, data) => axios.put(`${API_URL}/${id}`, data);
-export const deleteZona = (id) => axios.delete(`${API_URL}/${id}`);
+
+export const createZona = (data) => 
+    axios.post(API_URL, data, {headers: getAuthHeaders() });
+export const updateZona = (id, data) => axios.put(`${API_URL}/${id}`, data, { headers: getAuthHeaders()});
+export const deleteZona = (id) => axios.delete(`${API_URL}/${id}`, {headers: getAuthHeaders()});
