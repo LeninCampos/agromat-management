68baad1b9e47c2afa9ebce48d7195387bdb49c23
Manuel Bonavena <manuelbona1005@gmail.com>
2025-11-26 12:22:29 -0600

    Fix: actualización final del archivo db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,27 +2,13 @@
 import { Sequelize } from "sequelize";
 import "dotenv/config";
 
-<<<<<<< HEAD
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  // Soporta ambas variables para NO romper a nadie
-  process.env.DB_PASSWORD || process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 3306,
-    dialect: "mariadb",
-    logging: false
-  }
-);
-=======
 const DB_NAME = process.env.DB_NAME;
 const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD || process.env.DB_PASS || "";
 const DB_HOST = process.env.DB_HOST || "127.0.0.1";
 const DB_PORT = Number(process.env.DB_PORT || 3306);
 
-// Solo para debug (no imprimimos la password)
+// Solo para debug
 console.log("[DB] Conectando a:", {
   DB_NAME,
   DB_USER,
@@ -37,6 +23,5 @@ const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   dialect: "mariadb",
   logging: false,
 });
->>>>>>> 471eed5 (Fix: conexión a MariaDB con .env (DB_NAME, DB_USER, DB_PASSWORD))
 
 export default sequelize;
